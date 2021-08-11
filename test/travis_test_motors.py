d602de2015aad0a16ca48760a0b387bb7848a7e5
kyab <kyossi212@gmail.com>
2021-08-11 10:14:48 +0000

    add action

--- test/travis_test_motors.py
+++ test/travis_test_motors.py
@@ -70,8 +70,7 @@ class MotorTest(unittest.TestCase):
         self.assertEqual(ret.message, "ON", "motor on wrong message")
         with open("/dev/rtmotoren0", "r") as f:
             data = f.readline()
-            self.assertEqual(
-                data, "1\n", "wrong value in rtmotoren0 at motor on")
+            self.assertEqual(data, "1\n", "wrong value in rtmotoren0 at motor on")
 
     def test_put_value_timed(self):
         tm = rospy.ServiceProxy("/timed_motion", TimedMotion)
